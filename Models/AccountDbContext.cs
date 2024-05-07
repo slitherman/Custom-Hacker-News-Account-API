1456bbc64e31a370d3176fb60b134c31439df4c1
slitherman <ericcartman2700@gmail.com>
2024-05-07 15:38:26 +0200

    changed conn string

--- Models/AccountDbContext.cs
+++ Models/AccountDbContext.cs
@@ -25,7 +25,7 @@ public partial class AccountDbContext : DbContext
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseSqlServer("Server=localhost;Database=AccountDb;User Id=falasema4;Password=azhkvck4433;TrustServerCertificate=True");
+        => optionsBuilder.UseSqlServer("Data Source=LAPTOP-24E46FOR;Initial Catalog=AccountDb;User ID=falasema4;Password=azhkvck4433;Connect Timeout=30;Encrypt=True;TrustServerCertificate=True");
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
