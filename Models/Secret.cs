905a629db27b05d3247af034bd1f0def6d5934b6
slitherman <ericcartman2700@gmail.com>
2024-06-27 19:32:50 +0200

    Added Connstring using hidden class

--- Models/Secret.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Custom_Hacker_News_Account_API.Models
-{
-    public static class Secret
-    {
-        public static string Conn = "Server=tcp:hnprojserver.database.windows.net,1433;Initial Catalog=AccountDb;Persist Security Info=False;User ID=HNAdmin;Password=Superadmin4433;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
-    }
-}
